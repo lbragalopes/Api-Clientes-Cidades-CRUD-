6348c115976dd95bf13829a1618468a337809bb6
Luciana Lopes <lbragalopes@gmail.com>
2022-01-09 21:46:17 -0300

    Atualização

---
+++ ApiCliente.Infrastructure/Data/Repository/EF/Configuration/CidadeEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ApiCliente.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCliente.Infrastructure.Data.Repository.EF.Configuration
+{
+    class CidadeEntityConfiguration : IEntityTypeConfiguration<Cidade>
+    {
+        public void Configure(EntityTypeBuilder<Cidade> builder)
+        {
+            builder.ToTable("Cidade");
+            builder.Property(p => p.Nome).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Estado).IsRequired();
+           
+        }
+    }
+}
---
+++ ApiCliente.Infrastructure/Data/Repository/EF/Configuration/ClienteEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ApiCliente.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCliente.Infrastructure.Data.Repository.EF.Configuration
+{
+    public class ClienteEntityConfiguration : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("Cliente");
+            builder.Property(p => p.Nome).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.DataNascimento).IsRequired();
+            builder.Property(p => p.Cep).IsRequired();
+        }
+    }
+}
+
